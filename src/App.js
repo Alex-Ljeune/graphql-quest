a99fa4e4f7a20fbf5834636dc8fd59d92b535149
Alexis Roger <a.roger37@gmail.com>
2021-04-22 12:00:53 +0200

    requete à l'api spaceX réussie

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  gql,
+  useQuery,
+} from "@apollo/client";
+
+const LAUNCHES = gql`
+  query GetLaunches {
+    launches(limit: 5) {
+      launch_date_utc
+      launch_success
+      rocket {
+        rocket_name
+      }
+      links {
+        video_link
+      }
+      details
+    }
+  }
+`;
+
+function Launches() {
+  const { loading, error, data } = useQuery(LAUNCHES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+    console.log(data.launches)
+
+  return data.launches.map((launch,i) => (
+    < div key ={i}>
+      <h2>{launch.rocket['rocket_name']}</h2>
+      <p>
+        Mission name: {launch["mission_name"]}
+      </p>
+      <p>
+        Launch date: {launch["launch_date_utc"]}
+      </p>
+      <p>
+        Details {launch.details}
+      </p>
+      <p>
+        Launch success: {launch["launch_success"]}
+      </p>
+    </div >
+  ));
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <Launches />
     </div>
   );
 }
